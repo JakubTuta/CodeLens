4bb237f9a51cc5479983000f7181bd91e7e17433
JakubTuta <jakubtutka02@gmail.com>
2025-10-26 18:16:57 +0100

    docker-compose

--- backend/main.py
+++ backend/main.py
@@ -2,21 +2,24 @@ import os
 
 import fastapi
 from api.routes import router as websocket_router
-from fastapi.middleware.cors import CORSMiddleware
+
+# from fastapi.middleware.cors import CORSMiddleware
 
 
 def create_app():
     app = fastapi.FastAPI(title="CodeLens")
 
-    cors_origins = os.getenv("CORS_ORIGINS", "http://localhost:3000").split(",")
+    # CORS managed by Nginx
 
-    app.add_middleware(
-        CORSMiddleware,
-        allow_origins=cors_origins,
-        allow_credentials=True,
-        allow_methods=["*"],
-        allow_headers=["*"],
-    )
+    # cors_origins = os.getenv("CORS_ORIGINS", "http://localhost:3000").split(",")
+
+    # app.add_middleware(
+    #     CORSMiddleware,
+    #     allow_origins=cors_origins,
+    #     allow_credentials=True,
+    #     allow_methods=["*"],
+    #     allow_headers=["*"],
+    # )
 
     app.include_router(websocket_router)
 
